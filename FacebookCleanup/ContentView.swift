904eb74c3c3fd253dd3b06281a227fd63e43f196
Rob Haining <rob@tolar.ws>
2020-06-09 12:30:11 -0400

    Initial Commit

---
+++ FacebookCleanup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookCleanup
+//
+//  Created by Robert Tolar Haining on 6/9/20.
+//  Copyright © 2020 Robert Tolar Haining. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
